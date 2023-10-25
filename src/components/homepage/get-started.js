041a06f43d6c8fe62efdaac74161f73beefe6dea
ngud-0119 <finaldev.dream119@gmail.com>
2023-10-25 05:12:43 -0400

    first page pixel-perfect

--- src/components/homepage/get-started.js
+++ src/components/homepage/get-started.js
@@ -1,68 +1,72 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './homeNavBoxes.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./homeNavBoxes.module.css";
 
 const FeatureList = [
   {
-    title: 'What is Ceramic?',
+    title: "What is Ceramic?",
     items: [
-      {url: "docs/intro", text: "Dive into the core concepts behind the Ceramic Network"},
-    ]
+      {
+        url: "docs/intro",
+        text: "Dive into the core concepts behind the Ceramic Network",
+      },
+    ],
   },
   {
-    title: 'ComposeDB Sandbox',
+    title: "ComposeDB Sandbox",
     items: [
-      {url: "https://composedb.js.org/sandbox", text: "Interact with data stored in the Ceramic Network right in your browser."},
-    ]
+      {
+        url: "https://composedb.js.org/sandbox",
+        text: "Interact with data stored in the Ceramic Network right in your browser.",
+      },
+    ],
   },
   {
-    title: 'Ceramic Ecosystem',
+    title: "Ceramic Ecosystem",
     items: [
-      {url: "https://threebox.notion.site/Ceramic-Ecosystem-Directory-a3a7a58f81544d33ad3feb84368775d4", text: "Explore the innocative projects and tools build on Ceramic Network."},
-    ]
+      {
+        url: "https://threebox.notion.site/Ceramic-Ecosystem-Directory-a3a7a58f81544d33ad3feb84368775d4",
+        text: "Explore the innocative projects and tools build on Ceramic Network.",
+      },
+    ],
   },
- 
 ];
 
-function FeatureItem({url, text}){
+function FeatureItem({ url, text }) {
   return (
-    <li><a className={styles.listContainerLink} href={url}>{text}</a></li>
+    <>
+      <div className={styles.listContainerLink}>{text}</div>
+
+      <a className={styles.action} href={url}>
+        Learn more{" "}
+      </a>
+    </>
   );
 }
 
-
-function Feature({title, icon, items }) {
-
-
+function Feature({ title, icon, items }) {
   return (
-    <article className={clsx('col col--4')}>
+    <article className={clsx("col--4")}>
       <div className={styles.homecard}>
-        <h2>{title}</h2>
-        <div className={styles.listContainer}>
-        <ul>
-          {items.map((props, idx) => (
-            <FeatureItem key={idx} {...props} />
-          ))}
-        </ul>
-        </div>
+        <div className={styles.title}>{title}</div>
+
+        {items.map((props, idx) => (
+          <FeatureItem key={idx} {...props} />
+        ))}
+
       </div>
-      
     </article>
   );
 }
 
-
-
-
-
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
-        <ul className={styles.grid3col}>
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </ul>
+      <div className={styles.grid3col}>
+        {FeatureList.map((props, idx) => (
+          <Feature key={idx} {...props} />
+        ))}
+      </div>
     </section>
   );
 }
