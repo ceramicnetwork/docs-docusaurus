1edc43c9362a9e57a79764640d0888401eb09fc8
Justina Petraityte <justina.petraityte@gmail.com>
2023-11-21 15:19:30 +0000

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -50,9 +50,9 @@ const config: Config = {
 
   themeConfig: {
     algolia: {
-    appId: '3UBUL6C1TL',
-    apiKey: 'c80ef08afbfdb5f44dd3812dd7f72242',
-    indexName: 'developers-ceramic',
+      appId: '3UBUL6C1TL',
+      apiKey: '95ff5c5b36bcda0da231be4adcaa56e1',
+      indexName: 'developers-ceramic',
     },
     navbar: {
       title: 'Ceramic Documentation',
