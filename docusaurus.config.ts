15b864d2157cfa35d676552b7357c2bfc095a6c6
Justina Petraityte <justina.petraityte@gmail.com>
2023-12-06 18:39:30 +0000

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,7 +104,7 @@ const config: Config = {
                   '/reference/self-id/classes/core.PublicID/', '/reference/self-id/classes/core.Core/', '/reference/self-id/classes/web.WebClient/', 
                   '/reference/typescript/interfaces/_ceramicnetwork_core.IndexModelArgs.html', '/reference/typescript/interfaces/_ceramicnetwork_core.ceramicnetworkoptions.html', 
                   '/reference/javascript/packages/', '/reference/glaze/classes/devtools.ModelManager/', '/reference/self-id/classes/react.RequestClient/',
-                  '/reference/typescript/interfaces/_ceramicnetwork_common.AnchorValidator.html', '/reference/typescript/DEVELOPMENT.md'],
+                  '/reference/typescript/interfaces/_ceramicnetwork_common.AnchorValidator.html', '/reference/typescript/DEVELOPMENT.md', '/reference/typescript/docs-dev/UPGRADING.md'],
           },
           {
             to: '/docs/protocol/js-ceramic/streams/event-log',
