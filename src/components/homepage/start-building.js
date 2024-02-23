483f66ffdf4f3b25b86dcce439c56ae0a2586479
Radek Sienkiewicz <mail@velvetshark.com>
2024-02-23 03:31:53 +0100

    New Docs main page

--- src/components/homepage/start-building.js
+++ src/components/homepage/start-building.js
@@ -1,67 +1,50 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './homeNavBoxes.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./homeNavBoxes.module.css";
 
 const FeatureList = [
   {
-    title: 'ComposeDB Example App',
+    title: "Example App →",
     items: [
-      {url: "https://github.com/ceramicstudio/ComposeDbExampleApp", text: "Setup a fully functioning Ceramic app by running one simple command."},
+      {
+        url: "/docs/composedb/create-ceramic-app",
+        text: "Setup a fully functioning Ceramic app by running one simple command."
+      }
     ]
   },
   {
-    title: 'ComposeDB',
+    title: "ComposeDB →",
     items: [
-      {url: "docs/composedb/getting-started", text: "Build composable dApps using a decentralised graph database."},
+      { url: "docs/composedb/getting-started", text: "Build composable dApps using a decentralised graph database." }
     ]
   },
   {
-    title: 'Orbis',
-    items: [
-      {url: "https://docs.useorbis.com/", text: "Easily add Web3 social features to your application."},
-    ]
-  },
- 
+    title: "Orbis →",
+    items: [{ url: "https://docs.useorbis.com/", text: "Easily add Web3 social features to your application." }]
+  }
 ];
-
-function FeatureItem({url, text}){
-  return (
-    <>
-      <div className={styles.listContainerLink}>{text}</div>
-
-      <a className={styles.action} href={url}>
-        Learn more{" "}
-      </a>
-    </>
-  );
-}
-
 
 function Feature({ title, icon, items }) {
   return (
-    <article className={clsx("col--4")}>
-      <div className={styles.homecard}>
-        <div className={styles.title}>{title}</div>
-        
-          {items.map((props, idx) => (
-            <FeatureItem key={idx} {...props} />
-          ))}
-      
-      </div>
+    <article>
+      {items.map((item, idx) => (
+        <a key={idx} href={item.url} className={styles.homecardLink}>
+          <div className={styles.homecard}>
+            <div className={styles.title}>{title}</div>
+            <div className={styles.listContainerLink}>{item.text}</div>
+          </div>
+        </a>
+      ))}
     </article>
   );
 }
 
-
-
 export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
-        <div className={styles.grid3col}>
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
+      {FeatureList.map((props, idx) => (
+        <Feature key={idx} {...props} />
+      ))}
     </section>
   );
 }
